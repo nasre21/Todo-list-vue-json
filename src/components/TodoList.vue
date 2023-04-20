92076f2a179b8ac2500029507e80b0f1e47321a4
nasre21 <nasou2001@gmail.com>
2023-04-20 14:16:56 +0200

    acabar el proyecto

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,39 @@
 <template>
     <h1>Lista de tareas</h1>
     <div v-for="info in data.datos" :key="info.id">
-        <h4>{{ info.titulo }}</h4>
+        <h4>{{ info.g }}</h4>
         <p>{{ info.quien }}</p>
         <p>{{ info.description }}</p>
+        <div>
+            <button @click="buttonDelete(info.id)"> Delete</button>
+            <button @click="buttonEditar(info.id)"> Editar</button>
+            
+        </div>
     </div>
-  
+
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
+import axios from 'axios'
 defineProps({
     data:Object,
 })
+
+function buttonDelete(id){
+    
+        axios.delete(`http://localhost:3000/tareas/${id}`)
+        location.reload()   
+
+}
+
+function buttonEditar(id){
+    
+    axios.put(`http://localhost:3000/tareas/${id}`)
+    location.reload()
+
+}
+
 </script>
 
 <style>
